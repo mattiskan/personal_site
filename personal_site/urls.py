ce5387c75c823303262122e98065dad197317145
Mattis Kancans Envall <mattiskan@gmail.com>
2015-11-20 20:06:27 +0000

    added root redirect to /blog

--- personal_site/urls.py
+++ personal_site/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
+    url(r'^$', RedirectView.as_view(url='/blog', permanent=False), name='index'),
 ]

aed679a7d58f6bcd77bfc5099a1d0fb8d554a3c7
Mattis Kancans Envall <mattiskan@gmail.com>
2015-11-20 16:29:51 +0000

    added routing to blog/

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello world")
+
+
+def create(request):
+    return HttpResponse("This will create a new blog entry in the future!")

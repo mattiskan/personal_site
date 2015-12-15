09517f58b6d417b92bf6923845cb0527f4346674
Mattis Kancans Envall <mattiskan@gmail.com>
2015-12-15 12:00:29 +0100

    changed title of blog entries in admin interface

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class BlogEntry(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField(max_length=2000)
+
+    def __str__(self):
+        return self.title

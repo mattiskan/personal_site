712b7e37aaf6ae28409f6c00b5745a0a9ea7e25a
Mattis Kancans Envall <mattiskan@gmail.com>
2016-01-23 15:48:56 +0100

    changed how transforms are done

--- blog/views/index.py
+++ blog/views/index.py
@@ -8,32 +8,27 @@ from django.shortcuts import render
 from blog.models import BlogEntry
 from blog.api import EntryResource
 
-def transform(xml, xsl):
-    return etree.XSLT(etree.parse(xsl))(xml)
+
+
+def transform(xsl):
+    def wrap(function):
+        def transformed(*args, **kwargs):
+            output = etree.fromstring(''.join(function(*args, **kwargs)))
+            return HttpResponse(etree.XSLT(etree.parse(xsl))(output))
+        return transformed
+    return wrap
+
 
 def index(request):
-    feed_html = ''.join(
-        etree.tostring(
-            transform(
-                EntryResource.as_xml(request, entry_id=entry.id),
-                'blog/views/xsl/blog_feed.xsl'
-            )
-        )
-        for entry in BlogEntry.objects.order_by('-id')
-    )
-    
     return render(request, 'feed.html', context={
-        'feed_html': feed_html,
+        'entries': list(BlogEntry.objects.order_by('-id')),
     })
 
-
+@transform('blog/views/xsl/rss_feed.xsl')
 def rss(request):
-    rss_xml = etree.fromstring(Client().get('/blog/api/entry/').content)
+    return HttpResponse(Client().get('/blog/api/entry/').content)
 
-    transformed_xml = transform(rss_xml, 'blog/views/xsl/rss_feed.xsl')
-
-    return HttpResponse(etree.tostring(transformed_xml), content_type='text/xml')
-
+@transform('blog/views/xsl/id.xsl')
 def entry(request, entry_id):
     be = BlogEntry.objects.get(pk=entry_id)
 

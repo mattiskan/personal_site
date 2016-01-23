e61dd05fb7254fc20e6d02145e2811674c2f3b7f
Mattis Kancans Envall <dev@mattiskan.se>
2016-01-23 14:35:01 +0100

    added discourse

--- blog/views/index.py
+++ blog/views/index.py
@@ -35,15 +35,11 @@ def rss(request):
     return HttpResponse(etree.tostring(transformed_xml), content_type='text/xml')
 
 def entry(request, entry_id):
-    entry_html = etree.tostring(
-        transform(
-            EntryResource.as_xml(request, entry_id=entry_id),
-            'blog/views/xsl/blog_feed.xsl'
-        )
-    )
+    be = BlogEntry.objects.get(pk=entry_id)
 
     return render(request, 'entry.html',context={
-        'entry_html': entry_html,
+        'title': be.title,
+        'entry': be,
     })    
 
 def create(request):
